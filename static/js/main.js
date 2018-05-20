b6ef31c432c3331688c1992f94dffc96d1fa8798
Nicolás Parada <parada.nicolas@outlook.com>
2018-05-20 07:16:28 -0400

    router moved to npm package

--- static/js/main.js
+++ static/js/main.js
@@ -1,10 +1,10 @@
+import Router from 'https://unpkg.com/@nicolasparada/router@0.1.0/router.js';
 import { isAuthenticated } from './auth.js';
 import { importWithCache } from './dynamic-import.js';
-import Router from './router.js';
 
 const router = new Router()
 
-router.handle('/', guard(view('home'), view('welcome')))
+router.handle('/', guard(view('home')))
 router.handle('/callback', view('callback'))
 router.handle(/^\//, view('not-found'))
 
@@ -13,14 +13,13 @@ router.install(async resultPromise => {
     document.body.appendChild(await resultPromise)
 })
 
-
 function view(name) {
     return (...args) => importWithCache(`/js/pages/${name}-page.js`)
         .then(m => m.default)
         .then(h => h(...args))
 }
 
-function guard(fn1, fn2 = view('not-found')) {
+function guard(fn1, fn2 = view('welcome')) {
     return (...args) => isAuthenticated()
         ? fn1(...args)
         : fn2(...args)
