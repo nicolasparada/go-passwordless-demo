d36e83042a5f13d76cbe09c9c8ef40041095c36a
Nicolás Parada <parada.nicolas@outlook.com>
2021-05-02 18:44:00 -0400

    migrations: pgcrypto

--- repo/cockroach/migrations/migrate.sh
+++ repo/cockroach/migrations/migrate.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env sh
 
 force=false
-uuid_extension=false
+pgcrypto=false
 
 OPTIND=1
 
 while getopts 'f;e' opt; do
     case $opt in
         f) force=true ;;
-        e) uuid_extension=true;;
+        e) pgcrypto=true;;
         *) echo 'Error in command line parsing' >&2
             exit 1
     esac
@@ -21,8 +21,8 @@ fi
 
 cockroach sql --insecure -e "CREATE DATABASE IF NOT EXISTS passwordless"
 
-if "$uuid_extension"; then
-    cockroach sql --insecure -e "CREATE EXTENSION IF NOT EXISTS \"uuid-ossp\""
+if "$pgcrypto"; then
+    cockroach sql --insecure -e "CREATE EXTENSION IF NOT EXISTS \"pgcrypto\""
 fi
 
 cat $(dirname $0)/000_schema.sql | cockroach sql --insecure -d passwordless
